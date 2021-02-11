9c5e4ff638daf6d7217ad8120b103354c2ed91a4
Bernardo-Sousa1 <bernas_17_cs@hotmail.com>
2021-02-11 11:23:33 +0000

    Alterações no startup

--- UPtel/Startup.cs
+++ UPtel/Startup.cs
@@ -27,9 +27,12 @@ namespace UPtel
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
+            services.AddDbContext<UPtelContext>(options =>
                 options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                    Configuration.GetConnectionString("UPtelContext")));          
+            services.AddDbContext<ApplicationDbContext>(options =>
+                 options.UseSqlServer(
+                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
