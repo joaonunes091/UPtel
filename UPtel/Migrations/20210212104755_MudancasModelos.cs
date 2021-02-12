ac33cfbc1559736fa8ae9d673267c73e245cb026
David Pinto <Formacoesdavid@gmail.com>
2021-02-12 10:54:54 +0000

    Migração depois das alterações aos modelos

---
+++ UPtel/Migrations/20210212104755_MudancasModelos.cs
@@ -0,0 +1,195 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UPtel.Migrations
+{
+    public partial class MudancasModelos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Numero",
+                table: "Telefone",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(9)",
+                oldMaxLength: 9);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Telemovel",
+                table: "Funcionarios",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(9)",
+                oldMaxLength: 9);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Contribuinte",
+                table: "Funcionarios",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(9)",
+                oldMaxLength: 9);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CodigoPostalExt",
+                table: "Funcionarios",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(3)",
+                oldMaxLength: 3);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CodigoPostal",
+                table: "Funcionarios",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(4)",
+                oldMaxLength: 4);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CartaoCidadao",
+                table: "Funcionarios",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(8)",
+                oldMaxLength: 8);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Telefone",
+                table: "Clientes",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(9)",
+                oldMaxLength: 9,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Contribuinte",
+                table: "Clientes",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(9)",
+                oldMaxLength: 9);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CodigoPostal",
+                table: "Clientes",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(4)",
+                oldMaxLength: 4);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CartaoCidadao",
+                table: "Clientes",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(8)",
+                oldMaxLength: 8);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Numero",
+                table: "Telefone",
+                type: "nvarchar(9)",
+                maxLength: 9,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Telemovel",
+                table: "Funcionarios",
+                type: "nvarchar(9)",
+                maxLength: 9,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Contribuinte",
+                table: "Funcionarios",
+                type: "nvarchar(9)",
+                maxLength: 9,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CodigoPostalExt",
+                table: "Funcionarios",
+                type: "nvarchar(3)",
+                maxLength: 3,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CodigoPostal",
+                table: "Funcionarios",
+                type: "nvarchar(4)",
+                maxLength: 4,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CartaoCidadao",
+                table: "Funcionarios",
+                type: "nvarchar(8)",
+                maxLength: 8,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Telefone",
+                table: "Clientes",
+                type: "nvarchar(9)",
+                maxLength: 9,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Contribuinte",
+                table: "Clientes",
+                type: "nvarchar(9)",
+                maxLength: 9,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CodigoPostal",
+                table: "Clientes",
+                type: "nvarchar(4)",
+                maxLength: 4,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CartaoCidadao",
+                table: "Clientes",
+                type: "nvarchar(8)",
+                maxLength: 8,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
