af764f7bad8ce8494b6169a881172db9c49b7113
David Pinto <Formacoesdavid@gmail.com>
2021-02-12 10:28:03 +0000

    Alterações na tabela telefone

--- UPtel/Models/Telefone.cs
+++ UPtel/Models/Telefone.cs
@@ -20,22 +20,25 @@ namespace UPtel.Models
        
         [Required(ErrorMessage = "Campo de preenchimento obrigatório")]
         [Display(Name = "Número de telefone")]
-        [RegularExpression(@"(9[1236]|2\d)\d{7}", ErrorMessage = "Telefone Inválido")]
-        [StringLength(9, MinimumLength = 9)]
+        [RegularExpression(@"(2|1\d)\d{8}", ErrorMessage = "Telefone Inválido")]
+        //[StringLength(9, MinimumLength = 9)]
         public string Numero { get; set; }
 
         [Required(ErrorMessage = "Campo de preenchimento obrigatório")]
         [Display(Name = "Limite de minutos")]
+        [Range(0, 44600, ErrorMessage ="O número não é válido, o valor tem que estar entre 0 e 44640")]
         public int Limite { get; set; }
 
         [Required(ErrorMessage = "Campo de preenchimento obrigatório")]
         [Display(Name = "Preço por minuto de chamadas nacional")]
         [Column(TypeName = "decimal(4, 2)")]
+        [Range(0,99, ErrorMessage ="O valor não é válido")]
         public decimal PrecoMinutoNacional { get; set; }
 
         [Required(ErrorMessage = "Campo de preenchimento obrigatório")]
         [Display(Name = "Preço por minuto de chamadas internacionais")]
         [Column(TypeName = "decimal(4, 2)")]
+        [Range(0, 99, ErrorMessage = "O valor não é válido")]
         public decimal PrecoMinutoInternacional { get; set; }
 
         [InverseProperty("Telemovel")]
