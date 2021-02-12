5bf7268adadee00e1a5abf7de67935415bb6d159
Bernardo-Sousa1 <bernas_17_cs@hotmail.com>
2021-02-12 10:29:14 +0000

    ErrorMessage nos modelos clientes e telemovel

--- UPtel/Models/Telemovel.cs
+++ UPtel/Models/Telemovel.cs
@@ -20,36 +20,42 @@ namespace UPtel.Models
        
         [Required(ErrorMessage = "Campo de preenchimento obrigatório")]
         [Display(Name = "Número de telemóvel")]
-        [RegularExpression(@"(9[1236]|2\d)\d{7}", ErrorMessage = "Telefone Inválido")]
-        [StringLength(9, MinimumLength = 9)]
+        [RegularExpression(@"(9[1236]|2\d)\d{7}", ErrorMessage = "Número de telemóvel inválido")]
+        [StringLength(9, MinimumLength = 9,ErrorMessage ="Número de telemóvel inválido")]
         public string Numero { get; set; }
         
         [Required(ErrorMessage = "Campo de preenchimento obrigatório")]
         [Display(Name = "Limite de minutos")]
+        [Range(0,44640,ErrorMessage ="O número não é válido, o valor tem que estar entre 0 e 44640.")]
         public int LimiteMinutos { get; set; }
 
         [Required(ErrorMessage = "Campo de preenchimento obrigatório")]
         [Display(Name = "Limite de SMS")]
         [Column("LimiteSMS")]
+        [Range(0,10000,ErrorMessage ="O número não é válido, o valor tem que estar entre 0 e 10000")]
         public int LimiteSms { get; set; }
         [Required(ErrorMessage = "Campo de preenchimento obrigatório")]
         [Display(Name = "Preço por minuto de chamadas nacional")]
         [Column(TypeName = "decimal(4, 2)")]
+        [Range(0,99,ErrorMessage ="Esse valor não é válido")]
         public decimal PrecoMinutoNacional { get; set; }
 
         [Required(ErrorMessage = "Campo de preenchimento obrigatório")]
         [Display(Name = "Preço por minuto de chamadas internacionais")]
         [Column(TypeName = "decimal(4, 2)")]
+        [Range(0, 99, ErrorMessage = "Esse valor não é válido")]
         public decimal PrecoMinutoInternacional { get; set; }
 
         [Required(ErrorMessage = "Campo de preenchimento obrigatório")]
         [Display(Name = "Preço de SMS nacional")]
         [Column("PrecoSMS", TypeName = "decimal(4, 2)")]
+        [Range(0, 99, ErrorMessage = "Esse valor não é válido")]
         public decimal PrecoSms { get; set; }
 
         [Required(ErrorMessage = "Campo de preenchimento obrigatório")]
         [Display(Name = "Preço de MMS nacional")]
         [Column("PrecoMMS", TypeName = "decimal(4, 2)")]
+        [Range(0, 99, ErrorMessage = "Esse valor não é válido")]
         public decimal PrecoMms { get; set; }
 
         [InverseProperty("TelemovelNavigation")]
