c075d44950f15d4bb0ee9148aa167c7f47b53974
antoniocaldeira@yahoo.com <antoniocaldeira@yahoo.com>
2021-02-13 14:04:46 +0000

    Juntei seeddata

--- UPtel/Data/SeedData.cs
+++ UPtel/Data/SeedData.cs
@@ -8,6 +8,120 @@ namespace UPtel.Data
 {
     public class SeedData
     {
+        private static void InsereDadosTesteTelevisao(UPtelContext DbContext)
+        {
+            if (!DbContext.Televisao.Any()) return;
+            DbContext.Televisao.AddRange(new Televisao[] {
+                new Televisao
+                {
+                    Nome = "Base"
+                },
+                new Televisao
+                {
+                    Nome = "Base + Informação"
+                },
+                new Televisao
+                {
+                    Nome = "Series e Filmes"
+                },
+                new Televisao
+                {
+                    Nome = "Up"
+                }
+            });
+            DbContext.SaveChanges();
+        }
+        private static void InsereDadosTesteTelefone(UPtelContext DbContext)
+        {
+            if (!DbContext.Telefone.Any()) return;
+            DbContext.Telefone.AddRange(new Telefone[] {
+                new Telefone
+                {
+                    Numero = "220000000",
+                    Limite = 3000,
+                    PrecoMinutoNacional = 0,
+                    PrecoMinutoInternacional = 0.1M
+                },
+                new Telefone
+                {
+                    Numero = "200100000",
+                    Limite = 44600,
+                    PrecoMinutoNacional = 0,
+                    PrecoMinutoInternacional = 0.1M
+                },
+                new Telefone
+                {
+                    Numero = "290100000",
+                    Limite = 44600,
+                    PrecoMinutoNacional = 0,
+                    PrecoMinutoInternacional = 0
+                }
+            });
+            DbContext.SaveChanges();
+        }
+        private static void InsereDadosTesteTelemovel(UPtelContext DbContext)
+        {
+            if (!DbContext.Telemovel.Any()) return;
+            DbContext.Telemovel.AddRange(new Telemovel[] {
+                new Telemovel
+                {
+                    Numero = "90010000",
+                    LimiteMinutos = 3000,
+                    LimiteSms = 3000,
+                    PrecoMinutoNacional = 0,
+                    PrecoMinutoInternacional = 0.1M,
+                    PrecoSms = 0.05M,
+                    PrecoMms = 0.1M
+                },
+                new Telemovel
+                {
+                    Numero = "90010001",
+                    LimiteMinutos = 44600,
+                    LimiteSms = 5000,
+                    PrecoMinutoNacional = 0,
+                    PrecoMinutoInternacional = 0.1M,
+                    PrecoSms = 0.05M,
+                    PrecoMms = 0.1M
+                },
+                new Telemovel
+                {
+                    Numero = "90010002",
+                    LimiteMinutos = 44600,
+                    LimiteSms = 5000,
+                    PrecoMinutoNacional = 44600,
+                    PrecoMinutoInternacional = 0,
+                    PrecoSms = 0.05M,
+                    PrecoMms = 0.1M
+                }
+            });
+            DbContext.SaveChanges();
+        }
+
+        private static void InsereDadosTestePromocoes(UPtelContext DbContext)
+        {
+            if (!DbContext.Telemovel.Any()) return;
+            DbContext.Promocoes.AddRange(new Promocoes[] {
+                new Promocoes
+                {
+                    NomePromocao = "Extra Nacional",
+                    Descricao = "Oferta de minutos nacionais"
+                },
+                new Promocoes
+                {
+                    NomePromocao = "Inter Extra",
+                    Descricao = "Oferta de minutos internacionais"
+
+                },
+                new Promocoes
+                {
+                    NomePromocao = "Up Top",
+                    Descricao = "VIP only!... Comunicações grátis!"
+
+                }
+            });
+            DbContext.SaveChanges();
+        }
+
         private static void InsereDadosTesteCargos(UPtelContext DbContext)
         {
             if (!DbContext.Cargos.Any()) return;
@@ -298,6 +412,10 @@ namespace UPtel.Data
             InsereDadosTesteClientes(DbContext);
             InsereDadosTesteContratos(DbContext);
             InsereDadosTesteNetfixa(DbContext);
+            InsereDadosTesteTelevisao(DbContext);
+            InsereDadosTesteTelefone(DbContext);
+            InsereDadosTesteTelemovel(DbContext);
+            InsereDadosTestePromocoes(DbContext);
 
         }
 
