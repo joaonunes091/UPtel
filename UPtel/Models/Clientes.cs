355784576fce0ef0c3a8528309c937bdaac9f7e4
Bernardo Sousa <Bernas_17_cs@hotmail.com>
2021-02-19 11:47:53 +0000

    Adição de "*" nos campos obrigatórios

--- UPtel/Models/Clientes.cs
+++ UPtel/Models/Clientes.cs
@@ -21,36 +21,36 @@ namespace UPtel.Models
         public int ClienteId { get; set; }
 
         [Required(ErrorMessage = "Campo de preenchimento obrigatório")]
-        [Display(Name = "Nome do Cliente *")]
+        [Display(Name = "Nome do Cliente")]
         [StringLength(80, ErrorMessage = "O limite de caracteres(80) foi ultrapassado")]
         public string NomeCliente { get; set; }
 
         [Required(ErrorMessage = "Campo de preenchimento obrigatório")]
         [Column(TypeName = "date")]
-        [Display(Name = "Data de Nascimento *")]
+        [Display(Name = "Data de Nascimento")]
         [DataType(DataType.Date)]
         public DateTime DataNascimento { get; set; }
 
         [Required(ErrorMessage = "Campo de preenchimento obrigatório")]
         [StringLength(8, MinimumLength = 8,ErrorMessage ="Este valor é inválido")]
         [RegularExpression(@"\d{8}",ErrorMessage ="Este valor não é válido")]
-        [Display(Name = "Número do Cartão de Cidadão *")]
+        [Display(Name = "Número do Cartão de Cidadão")]
         public string CartaoCidadao { get; set; }
 
         [Required(ErrorMessage = "Campo de preenchimento obrigatório")]
         [StringLength(9, MinimumLength = 9,ErrorMessage ="Este valor é inválido")]
         [RegularExpression(@"\d{9}",ErrorMessage ="Este valor é inválido")]
-        [Display(Name = "Número de Contribuinte *")]
+        [Display(Name = "Número de Contribuinte")]
         public string Contribuinte { get; set; }
 
         [Required(ErrorMessage = "Campo de preenchimento obrigatório")]
         [StringLength(80, ErrorMessage = "O limite de caracteres(80) foi ultrapassado")]
-        [Display(Name ="Morada *")]
+        [Display(Name ="Morada")]
         public string Morada { get; set; }
 
         [Required(ErrorMessage = "Campo de preenchimento obrigatório")]
         [StringLength(4, MinimumLength = 4)]
-        [Display(Name = "Código Postal *")]
+        [Display(Name = "Código Postal")]
         [RegularExpression(@"([123456789]|1)\d{3}",ErrorMessage ="Valor inválido")]
         public string CodigoPostal { get; set; }
 
@@ -62,13 +62,13 @@ namespace UPtel.Models
         [Required(ErrorMessage = "Campo de preenchimento obrigatório")]
         [StringLength(9, MinimumLength = 9)]
         [RegularExpression(@"(9[1236]|2\d)\d{7}", ErrorMessage = "Telefone Inválido")]
-        [Display(Name = "Número de telemóvel *")]
+        [Display(Name = "Número de telemóvel")]
         public string Telemovel { get; set; }
 
         [Required(ErrorMessage = "Campo de preenchimento obrigatório")]
         [EmailAddress]
         [StringLength(50, ErrorMessage = "O limite de caracteres(50) foi ultrapassado")]
-        [Display(Name = "E-mail *")]
+        [Display(Name = "E-mail")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Campo de preenchimento obrigatório")]
