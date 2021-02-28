77120d09cd72ee5dccaa6046ca69934f0b56bb6b
andre saavedra <andre.saavedra@hotmail.com>
2021-02-28 21:53:48 +0000

    Criação de uma página para mostrar as informações dos clientes

---
+++ UPtel/Models/ClientesViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UPtel.Models
+{
+    public class ClientesViewModel
+    {
+        public int Id { get; set; }
+
+        //Dados Pessoais
+
+        public string NomeCliente { get; set; }
+
+        public DateTime DataNascimento { get; set; }
+
+        public string CartaoCidadao { get; set; }
+
+        public string NumeroContribuinte { get; set; }
+
+        public string Morada { get; set; }
+
+        public string CodiogoPostal { get; set; }
+
+        public string ExtensaoCodigoPostal { get; set; }
+
+        public string Telefone { get; set; }
+
+        public string Telemovel { get; set; }
+
+        public string Email { get; set; }
+
+        //Informação contrato
+
+        //public string NomeFuncionario { get; set; }
+
+        //public DateTime DataInicio { get; set; }
+
+        //public int? Fidelizacao { get; set; }
+
+        //public int? TempoPromocao { get; set; }
+
+        //public decimal PrecoContrato { get; set; }
+    }
+}
