e837adf8d1ac8396e7bb574be2df7447714438c8
David Pinto <Formacoesdavid@gmail.com>
2021-03-01 14:38:16 +0000

    Validação Nif registo operador

--- UPtel/Models/RegistoUserViewModel.cs
+++ UPtel/Models/RegistoUserViewModel.cs
@@ -66,7 +66,7 @@ namespace UPtel.Models
         //[Required(ErrorMessage = "Campo de preenchimento obrigatório")]
         [DataType(DataType.Password)]
         [StringLength(16)]
-        [Compare("Password",ErrorMessage ="A passord não coincide.")]
+        [Compare("Password",ErrorMessage ="A password não coincide.")]
         public string ConfirmePassword { get; set; }
 
         [Display(Name = "Tipo de cliente")]
