fe3ccc6d8b2d2395872ae91e25d21f32811980da
antoniocaldeira@yahoo.com <antoniocaldeira@yahoo.com>
2021-03-01 19:32:54 +0000

    Criação de método para validação de contribuinte

---
+++ UPtel/Data/ValidationFiscalNumber.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UPtel.Data
+{
+    public class ValidationFiscalNumber
+    {
+        private bool ValidateFicalNumber(string nif)
+        {
+            
+            char firstChar = nif[0];
+            if (firstChar.Equals('1')
+                || firstChar.Equals('2')
+                || firstChar.Equals('3')
+                || firstChar.Equals('5')
+                || firstChar.Equals('6')
+                || firstChar.Equals('8')
+                || firstChar.Equals('9'))
+            {
+                int checkDigit = (Convert.ToInt32(firstChar.ToString()) * 9);
+                for (int i = 2; i <= 8; ++i)
+                {
+                    checkDigit += Convert.ToInt32(nif[i - 1].ToString()) * (10 - i);
+                }
+
+                checkDigit = 11 - (checkDigit % 11);
+                if (checkDigit >= 10)
+                {
+                    checkDigit = 0;
+                }
+
+                if (checkDigit.ToString() != nif[8].ToString())
+                {
+                   return false;
+                }
+            }
+            return true;
+        }
+    }
+}
