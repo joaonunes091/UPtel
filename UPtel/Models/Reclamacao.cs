f5d2a14de5699305fd45da74dc9fcb8d2aa62db8
andre saavedra <andre.saavedra@hotmail.com>
2021-03-16 12:38:34 +0000

    Criação do modelo de reclamação e edição do controlador e view create do modelo reclamação

---
+++ UPtel/Models/Reclamacao.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UPtel.Models
+{
+    public class Reclamacao
+    {
+        public int ReclamacaoId { get; set; }
+
+        public string Assunto { get; set; }
+
+        public string Descriçao { get; set; }
+
+        public int ClienteId { get; set; }
+
+        public bool Resolvido { get; set; }
+    }
+}
