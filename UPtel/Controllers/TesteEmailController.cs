63b400657af27205df7eaaa0b22c660738abede9
Bernardo Sousa <Bernas_17_cs@hotmail.com>
2021-03-27 22:17:21 +0000

    Adição da funcionalidade do email, falta corrigir um erro que está a enviar para a vista errada após o envio do email

---
+++ UPtel/Controllers/TesteEmailController.cs
@@ -0,0 +1,61 @@
+﻿using UPtel.Services;
+using UPtel.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UPtel.Controllers
+{
+    public class TesteEmailController : Controller
+    {
+        private readonly IEmailSender _emailSender;
+        public TesteEmailController(IEmailSender emailSender, IWebHostEnvironment env)
+        {
+            _emailSender = emailSender;
+        }
+        public IActionResult EnviaEmail()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult EnviaEmail(EmailModel email)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    TesteEnvioEmail(email.Destino, email.Assunto, email.Mensagem).GetAwaiter();
+                    return RedirectToAction("EmailEnviado");
+                }
+                catch (Exception)
+                {
+                    return RedirectToAction("EmailFalhou");
+                }
+            }
+            return View(email);
+        }
+        public async Task TesteEnvioEmail(string email, string assunto, string mensagem)
+        {
+            try
+            {
+                //email destino, assunto do email, mensagem a enviar
+                await _emailSender.SendEmailAsync(email, assunto, mensagem);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public ActionResult EmailEnviado()
+        {
+            return View();
+        }
+        public ActionResult EmailFalhou()
+        {
+            return View();
+        }
+    }
+}
