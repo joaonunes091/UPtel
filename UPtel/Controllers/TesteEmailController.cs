0f02ba8b51283b31f9eaff40b6c245145864918e
Bernardo Sousa <Bernas_17_cs@hotmail.com>
2021-04-09 06:56:12 +0100

    Emails quase automáticod

--- UPtel/Controllers/TesteEmailController.cs
+++ UPtel/Controllers/TesteEmailController.cs
@@ -6,56 +6,161 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UPtel.Data;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace UPtel.Controllers
 {
     public class TesteEmailController : Controller
     {
         private readonly IEmailSender _emailSender;
-        public TesteEmailController(IEmailSender emailSender, IWebHostEnvironment env)
+        private readonly UPtelContext bd;
+        public TesteEmailController(IEmailSender emailSender, IWebHostEnvironment env, UPtelContext context)
         {
             _emailSender = emailSender;
+            bd = context;
         }
         public IActionResult EnviaEmail()
         {
             return View();
         }
-        [HttpPost]
-        public IActionResult EnviaEmail(EmailModel email)
+        //[HttpPost]
+        //public IActionResult EnviaEmail(EmailModel email)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+        //            TesteEnvioEmail(email.Destino, email.Assunto, email.Mensagem).GetAwaiter();
+        //            return RedirectToAction("EmailEnviado");
+        //        }
+        //        catch (Exception)
+        //        {
+        //            return RedirectToAction("EmailFalhou");
+        //        }
+        //    }
+        //    return View(email);
+        //}
+
+        public async Task<IActionResult> TesteEnvioEmail()
         {
-            if (ModelState.IsValid)
+            var contratos = await bd.Contratos.ToListAsync();
+            var emailenviado = await bd.EnvioDeFaturas.ToListAsync();
+
+            DateTime hoje = DateTime.Today;
+            DateTime mespassado = hoje.AddMonths(-1);
+            int mes = mespassado.Month;
+
+            foreach (var item in emailenviado)
             {
+                if (item.mes == mes)
+                {
+                    return RedirectToAction("EmailsJaEnviados");
+                }
+            }
+
+
+            string email; string assunto; string mensagem;
+
+
+            foreach (var item in *)
+            {
+                //var cliente = await bd.Users.FirstOrDefaultAsync(m => m.UserId == item.UserId);
+                decimal preco = *;
+                email = cliente.Email;
+
+                string NomeMes = NomesDoMes(mes);
+                assunto = "UPtel - Faturação de "+ NomeMes;
+                mensagem = "Caro/a cliente, informamos que o preço a pagar em " + NomeMes + "é de" + preco + "€ da fatura de ";
+
                 try
                 {
-                    TesteEnvioEmail(email.Destino, email.Assunto, email.Mensagem).GetAwaiter();
-                    return RedirectToAction("EmailEnviado");
+                    //email destino, assunto do email, mensagem a enviar
+                    await _emailSender.SendEmailAsync(email, assunto, mensagem);
+
+
                 }
                 catch (Exception)
                 {
                     return RedirectToAction("EmailFalhou");
                 }
             }
-            return View(email);
-        }
-        public async Task TesteEnvioEmail(string email, string assunto, string mensagem)
-        {
-            try
+
+            emailenviado.Add(new EnvioDeFaturas() { DataDeEnvio = DateTime.Today, Enviado = true, mes = mes });
+            foreach (var item in emailenviado)
             {
-                //email destino, assunto do email, mensagem a enviar
-                await _emailSender.SendEmailAsync(email, assunto, mensagem);
+                bd.EnvioDeFaturas.Add(item);
             }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
+            await bd.SaveChangesAsync();
+            return RedirectToAction("EmailEnviado");
+
+
+
         }
         public ActionResult EmailEnviado()
+        {
+            return View();
+        }
+        public ActionResult EmailsJaEnviados()
         {
             return View();
         }
         public ActionResult EmailFalhou()
         {
             return View();
+        }
+
+        internal static string NomesDoMes(int mes)
+        {
+            string NomedoMes = "";
+            switch (mes)
+            {
+                case 1:
+                    NomedoMes = "Janeiro";
+                    break;
+                case 2:
+                    NomedoMes = "Fevereiro";
+                    break;
+                case 3:
+                    NomedoMes = "Março";
+                    break;
+                case 4:
+                    NomedoMes = "Abril";
+                    break;
+                case 5:
+                    NomedoMes = "Maio";
+                    break;
+                case 6:
+                    NomedoMes = "Junho";
+                    break;
+                case 7:
+                    NomedoMes = "Julho";
+                    break;
+                case 8:
+                    NomedoMes = "Agosto";
+                    break;
+                case 9:
+                    NomedoMes = "Setembro";
+                    break;
+                case 10:
+                    NomedoMes = "Outubro";
+                    break;
+                case 11:
+                    NomedoMes = "Novembro";
+                    break;
+                case 12:
+                    NomedoMes = "Dezembro";
+                    break;
+
+                default:
+                    break;
+            }
+
+            return NomedoMes;
         }
     }
 }
